0c12b7df3fe414c6b20da7008356732fa85c4c28
Licheng Lin <coderlin007@163.com>
2022-08-30 17:08:42 +0800

    complete build chain

--- chain-demo/src/chain/build.rs
+++ chain-demo/src/chain/build.rs
@@ -1,29 +1,32 @@
+use log::info;
+
+use crate::Digest;
 use super::*;
 
 
 pub fn build_block<'a>(
     block_id: IdType,
     pre_hash: Digest,
-    raw_txs: impl Iterator<Item = &'a RawObject>,
+    raw_txs: impl Iterator<Item = &'a RawTransaction>,
     chain: &mut (impl ReadInterface + WriteInterface),
 ) -> Result<BlockHeader> {
     info!("Build block {}", block_id);
 
     let param = chain.get_parameter()?;
     let txs: Vec<Transaction> = raw_txs.map(|rtx: &RawTransaction| Transaction::create(rtx)).collect();
-    let timestamp: TsType = Default::default();
+    let mut time_stamp: TsType = Default::default();
     for tx in txs{
-        timestamp = tx.value.time_stamp; // not good enough
+        time_stamp = tx.value.time_stamp; // not good enough
         chain.write_transaction(tx.clone());
     }
 
-    let pub_key: PkType = String::from("need to complete");
+    let public_key: PkType = String::from("need to complete");
     
     let mut block_header = BlockHeader{
         block_id,
         pre_hash,
-        timestamp,
-        pub_key,
+        time_stamp,
+        public_key,
     };
 
     // need to complete
@@ -31,8 +34,8 @@ pub fn build_block<'a>(
         block_id,
     };
 
-    chain.write_block_header(block_header)?;
-    chain.write_block_data(block_data)?;
+    chain.write_block_header(block_header.clone())?;
+    chain.write_block_data(block_data.clone())?;
 
     Ok(block_header)
 }
