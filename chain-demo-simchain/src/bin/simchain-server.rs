0c4dbe91c75d3022698dbfb0c2c1ccc6163a719e
Licheng Lin <coderlin007@163.com>
2022-08-31 17:50:22 +0800

    init simchain-server

--- chain-demo-simchain/src/bin/simchain-server.rs
+++ chain-demo-simchain/src/bin/simchain-server.rs
@@ -1,7 +1,23 @@
+use structopt::StructOpt;
+use std::path::PathBuf;
 
+
+#[derive(StructOpt, Debug)]
+#[structopt(name = "simchain-server")]
+struct Opts {
+    /// input db path
+    #[structopt(short = "-db", long, parse(from_os_str))]
+    db_path: PathBuf,
+
+    /// server binding address
+    #[structopt(short, long, default_value = "127.0.0.1:8000")]
+    binding: String,
+}
 
 #[actix_rt::main]
 async fn main() -> actix_web::Result<()> {
-    
+    env_logger::init_from_env(env_logger::Env::default().filter_or("RUST_LOG", "info"));
+    let opts = Opts::from_args();
+
     Ok(())
 }
