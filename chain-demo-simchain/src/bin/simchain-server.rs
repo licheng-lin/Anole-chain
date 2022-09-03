a2fa43bb427c41ef2ba9ea9c2f614a11b7ace3ab
Licheng Lin <coderlin007@163.com>
2022-09-03 20:11:55 +0800

    complete error & chain

--- chain-demo-simchain/src/bin/simchain-server.rs
+++ chain-demo-simchain/src/bin/simchain-server.rs
@@ -1,5 +1,30 @@
+use chain_demo_simchain::SimChain;
 use structopt::StructOpt;
 use std::path::PathBuf;
+use std::fmt;
+use actix_cors::Cors;
+use actix_web::{web, App, HttpResponse, HttpServer, Responder};
+
+static mut CHAIN: Option<SimChain> = None;
+
+fn get_chain() -> &'static SimChain{
+    unsafe{ CHAIN.as_ref().unwrap()}
+}
+
+#[derive(Debug)]
+struct MyErr(anyhow::Error);
+
+impl fmt::Display for MyErr {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(f, "error: {}", self.0.to_string())
+    }
+}
+impl actix_web::error::ResponseError for MyErr {}
+
+fn handle_err<E: fmt::Display + fmt::Debug + Send + Sync + 'static>(e: E) -> MyErr{
+    MyErr(anyhow::Error::msg(e))
+}
+
 
 
 #[derive(StructOpt, Debug)]
@@ -18,6 +43,7 @@ struct Opts {
 async fn main() -> actix_web::Result<()> {
     env_logger::init_from_env(env_logger::Env::default().filter_or("RUST_LOG", "info"));
     let opts = Opts::from_args();
-
+    let chain = SimChain::open(&opts.db_path).unwrap();
+    unsafe{CHAIN = Some(chain)};
     Ok(())
 }
