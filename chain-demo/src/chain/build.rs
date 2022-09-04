0195822aebd51283c42bfa2449dfb37a2cdfd96a
Licheng Lin <coderlin007@163.com>
2022-09-04 22:20:35 +0800

    integrate schnorr with serializable problem

--- chain-demo/src/chain/build.rs
+++ chain-demo/src/chain/build.rs
@@ -8,19 +8,20 @@ pub fn build_block<'a>(
     block_id: IdType,
     pre_hash: Digest,
     raw_txs: impl Iterator<Item = &'a RawTransaction>,
+    key_pair: Keypair,
     chain: &mut (impl ReadInterface + WriteInterface),
 ) -> Result<BlockHeader> {
     info!("Build block {}", block_id);
 
     let param = chain.get_parameter()?;
-    let txs: Vec<Transaction> = raw_txs.map(|rtx: &RawTransaction| Transaction::create(rtx)).collect();
+    let txs: Vec<Transaction> = raw_txs.map(|rtx: &RawTransaction| Transaction::create_with_sk(rtx, &key_pair)).collect();
     let mut time_stamp: TsType = Default::default();
     for tx in txs{
         time_stamp = tx.value.time_stamp; // not good enough
         chain.write_transaction(tx.clone())?;
     }
 
-    let public_key: PkType = String::from("need to complete");
+    let public_key: PkType = key_pair.public;
     
     let block_header = BlockHeader{
         block_id,
--- chain-demo/src/chain/transaction.rs
+++ chain-demo/src/chain/transaction.rs
@@ -1,26 +1,44 @@
 use std::sync::atomic::{AtomicU64, Ordering};
 
-use super::{IdType, TsType, Txtype};
-use crate::chain::*;
+use super::*;
 use serde::{Deserialize, Serialize};
 
 static TX_ID_CNT: AtomicU64 = AtomicU64::new(0);
 
-#[derive(Debug, Default, Clone, Eq, PartialEq, Serialize, Deserialize)]
+#[derive(Debug, Default, Clone, Eq, PartialEq)]
 pub struct TransactionValue {
     pub trans_in: bool,
     pub trans_value: Txtype,
     pub time_stamp: TsType,
 }
 
-#[derive(Debug, Default, Clone, Eq, PartialEq, Serialize, Deserialize)]
+impl TransactionValue{
+    pub fn to_string(&self) -> String{
+        let result =String::from(self.trans_in.to_string())
+        + &String::from(self.trans_value.to_string())
+        + &String::from(self.time_stamp.to_string());
+        result
+    }
+}
+#[derive(Debug, Default, Clone, Eq, PartialEq)]
 pub struct RawTransaction {
     pub block_id: IdType,
     pub key: KeyType,
     pub value: TransactionValue,
 }
 
-#[derive(Debug, Clone, Eq, PartialEq, Serialize, Deserialize)]
+impl RawTransaction{
+    fn sign<'a>(&self, key_pair:&'a Keypair) -> Signature {
+        let ctx = signing_context(b"");
+        let raw_data = String::from(&self.block_id.to_string())
+            + &String::from(&self.key)
+            + &String::from(&self.value.to_string());
+        
+        key_pair.sign(ctx.bytes(raw_data.as_bytes()))
+    }
+}
+
+#[derive(Debug, Clone, Eq, PartialEq)]
 pub struct Transaction {
     pub id: IdType,
     pub block_id: IdType,
@@ -31,13 +49,28 @@ pub struct Transaction {
 
 impl Transaction {
 
-    pub fn create(obj: &RawTransaction) -> Self {
+    // pub fn create(obj: &RawTransaction) -> Self {
+    //     let id = TX_ID_CNT.fetch_add(1, Ordering::SeqCst) as IdType;
+    //     let block_id = obj.block_id.clone();
+    //     // let signature = sign_transaction();
+    //     let signature = String::from("need to complete");
+    //     let key = obj.key.clone();
+    //     let value = obj.value.clone();
+    //     Self {
+    //         id,
+    //         block_id,
+    //         key,
+    //         value,
+    //         signature
+    //     }
+    // }
+    pub fn create_with_sk(tx: &RawTransaction, key_pair: &Keypair) -> Self {
         let id = TX_ID_CNT.fetch_add(1, Ordering::SeqCst) as IdType;
-        let block_id = obj.block_id.clone();
+        let block_id = tx.block_id.clone();
         // let signature = sign_transaction();
-        let signature = String::from("need to complete");
-        let key = obj.key.clone();
-        let value = obj.value.clone();
+        let signature = tx.sign(key_pair);
+        let key = tx.key.clone();
+        let value = tx.value.clone();
         Self {
             id,
             block_id,
@@ -49,31 +82,31 @@ impl Transaction {
 }
 
 
-#[cfg(test)]
-mod tests {
-    use super::*;
+// #[cfg(test)]
+// mod tests {
+//     use super::*;
 
-    #[test]
-    fn test_transaction_create() {
-        let raw_value = TransactionValue {
-            trans_in: true,
-            trans_value: 122,
-            time_stamp: 821,
-        };
-        let raw_transaction = RawTransaction{
-            block_id: 1,
-            key: String::from("76a914a57414e5ffae9ef5074bacbe10a320bb2614e1f388ac"),
-            value: raw_value.clone(),
-        };
-        let tx = Transaction::create(&raw_transaction);
-        assert_eq!(tx.signature,String::from("need to complete"));
-        let expected = Transaction {
-            id: 0,
-            block_id: 1,
-            key: String::from("76a914a57414e5ffae9ef5074bacbe10a320bb2614e1f388ac"),
-            value: raw_value,
-            signature: String::from("need to complete"),
-        };
-        assert_eq!(tx,expected);
-    }
-}
+//     #[test]
+//     fn test_transaction_create() {
+//         let raw_value = TransactionValue {
+//             trans_in: true,
+//             trans_value: 122,
+//             time_stamp: 821,
+//         };
+//         let raw_transaction = RawTransaction{
+//             block_id: 1,
+//             key: String::from("76a914a57414e5ffae9ef5074bacbe10a320bb2614e1f388ac"),
+//             value: raw_value.clone(),
+//         };
+//         let tx = Transaction::create(&raw_transaction);
+//         assert_eq!(tx.signature,String::from("need to complete"));
+//         let expected = Transaction {
+//             id: 0,
+//             block_id: 1,
+//             key: String::from("76a914a57414e5ffae9ef5074bacbe10a320bb2614e1f388ac"),
+//             value: raw_value,
+//             signature: String::from("need to complete"),
+//         };
+//         assert_eq!(tx,expected);
+//     }
+// }
