ec3debd04af97d963afc72f411ad7277fc944e1a
Licheng Lin <coderlin007@163.com>
2022-09-04 18:47:06 +0800

    fix tx write bug

--- chain-demo/src/chain/transaction.rs
+++ chain-demo/src/chain/transaction.rs
@@ -1,7 +1,10 @@
+use std::sync::atomic::{AtomicU64, Ordering};
+
 use super::{IdType, TsType, Txtype};
 use crate::chain::*;
 use serde::{Deserialize, Serialize};
 
+static TX_ID_CNT: AtomicU64 = AtomicU64::new(0);
 
 #[derive(Debug, Default, Clone, Eq, PartialEq, Serialize, Deserialize)]
 pub struct TransactionValue {
@@ -19,6 +22,7 @@ pub struct RawTransaction {
 
 #[derive(Debug, Clone, Eq, PartialEq, Serialize, Deserialize)]
 pub struct Transaction {
+    pub id: IdType,
     pub block_id: IdType,
     pub key: KeyType,
     pub value: TransactionValue,
@@ -28,12 +32,14 @@ pub struct Transaction {
 impl Transaction {
 
     pub fn create(obj: &RawTransaction) -> Self {
+        let id = TX_ID_CNT.fetch_add(1, Ordering::SeqCst) as IdType;
         let block_id = obj.block_id.clone();
         // let signature = sign_transaction();
         let signature = String::from("need to complete");
         let key = obj.key.clone();
         let value = obj.value.clone();
         Self {
+            id,
             block_id,
             key,
             value,
@@ -62,6 +68,7 @@ mod tests {
         let tx = Transaction::create(&raw_transaction);
         assert_eq!(tx.signature,String::from("need to complete"));
         let expected = Transaction {
+            id: 0,
             block_id: 1,
             key: String::from("76a914a57414e5ffae9ef5074bacbe10a320bb2614e1f388ac"),
             value: raw_value,
