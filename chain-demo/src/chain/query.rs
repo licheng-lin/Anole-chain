aa07c8dfde9b3181c9fb89ec69ad7ed57e6bd3d9
Licheng Lin <coderlin007@163.com>
2022-09-06 15:47:02 +0800

    query parameter

--- chain-demo/src/chain/query.rs
+++ chain-demo/src/chain/query.rs
@@ -1,6 +1,7 @@
 use super::*;
+use curve25519_dalek::scalar::Scalar;
 use serde::{Serialize, Deserialize};
-
+use std::collections::HashMap;
 #[derive(Debug, Default, Clone, Eq, PartialEq, Serialize, Deserialize)]
 pub struct QueryParam{
 
@@ -10,3 +11,18 @@ pub struct QueryParam{
     pub value: TsType, 
 
 }
+
+#[derive(Debug, Default, Clone, PartialEq, Eq, Serialize, Deserialize)]
+pub struct OverallResult{
+    #[serde(rename = "result")]
+    pub res_txs: ResultTxs,
+    pub query_param: QueryParam,
+    pub query_time_ms: u64,
+    pub use_index: bool,
+}
+
+#[derive(Debug, Default, Clone, PartialEq, Eq, Serialize, Deserialize)]
+pub struct ResultTxs(pub HashMap<AggregateSign, Vec<Transaction>>);
+
+#[derive(Debug, Default, Clone, PartialEq, Eq, Serialize, Deserialize, Hash)]
+pub struct AggregateSign(pub Scalar, pub Vec<CompressedRistretto>, pub CompressedRistretto);
