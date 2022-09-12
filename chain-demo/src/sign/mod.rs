47473e317a0219230121570eba4d257db440417e
Licheng Lin <coderlin007@163.com>
2022-09-12 11:51:28 +0800

    add struct AggregateSignature

--- chain-demo/src/sign/mod.rs
+++ chain-demo/src/sign/mod.rs
@@ -8,6 +8,8 @@ fn rand_hack() -> impl RngCore+CryptoRng {
 
 #[macro_use]
 pub mod serdey;
+use curve25519_dalek::{scalar::Scalar, ristretto::CompressedRistretto};
+use serde::{Serialize, Deserialize};
 pub use serdey::*;
 
 pub mod scalars;
@@ -34,10 +36,5 @@ pub use boscoster::*;
 pub mod sign;
 pub use sign::*;
 
-
-// pub use errors::{SignatureError,SignatureResult};
-// pub use context::{signing_context}; // SigningContext,SigningTranscript
-// pub use sign::{Signature,SIGNATURE_LENGTH,sign_aggregate};
-// pub use keys::*;
-// pub use batch::{verify_batch,verify_batch_rng,verify_batch_deterministic,PreparedBatch,verify_batch_direct,verify_batch_bos};
-// pub use sign::{PublicKey,Keypair,Signature, signing_context,verify_batch_bos,sign_aggregate,verify_batch};
+#[derive(Debug, Clone, Eq, PartialEq, Serialize, Deserialize)]
+pub struct AggregateSignature(Scalar, Vec<CompressedRistretto>, CompressedRistretto);
